9b02e40e4b4a048d5471c18deb7ef84400a114c9
KarolAlvarez <aalvarez3570@gmail.com>
2017-11-07 17:39:00 -0500

    Taller eliminar contacto + actualizar

--- app/src/main/java/com/example/jonmid/contactosbasededatos/Views/SearchContactActivity.java
+++ app/src/main/java/com/example/jonmid/contactosbasededatos/Views/SearchContactActivity.java
@@ -1,14 +1,17 @@
 package com.example.jonmid.contactosbasededatos.Views;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.jonmid.contactosbasededatos.ContactsActivity;
 import com.example.jonmid.contactosbasededatos.Helpers.SqliteHelper;
 import com.example.jonmid.contactosbasededatos.R;
 import com.example.jonmid.contactosbasededatos.Utilities.Constants;
@@ -19,6 +22,7 @@ public class SearchContactActivity extends AppCompatActivity {
     TextView textViewName;
     TextView textViewPhone;
     TextView textViewEmail;
+    EditText edtxtName, edtxtphone, edtxtEmail;
     SqliteHelper sqliteHelper;
 
     @Override
@@ -31,31 +35,57 @@ public class SearchContactActivity extends AppCompatActivity {
         textViewPhone = (TextView) findViewById(R.id.id_tv_search_phone);
         textViewEmail = (TextView) findViewById(R.id.id_tv_search_email);
 
+
+        edtxtName = (EditText) findViewById(R.id.id_et_name);
+        edtxtphone = (EditText) findViewById(R.id.id_et_phone);
+        edtxtEmail = (EditText) findViewById(R.id.id_et_email);
+
+
         sqliteHelper = new SqliteHelper(this, "db_users", null, 1);
     }
 
-    public void onClickSearchContact(View view){
+    public void onClickSearchContact(View view) {
         searchContact();
     }
 
-    public void searchContact(){
+    public void searchContact() {
         SQLiteDatabase db = sqliteHelper.getReadableDatabase();
 
         String[] params = {textViewParam.getText().toString()};
-        String[] fields = {Constants.TABLA_FIELD_NAME,Constants.TABLA_FIELD_PHONE,Constants.TABLA_FIELD_EMAIL};
+        String[] fields = {Constants.TABLA_FIELD_ID, Constants.TABLA_FIELD_NAME, Constants.TABLA_FIELD_PHONE, Constants.TABLA_FIELD_EMAIL};
 
 
         try {
-            Cursor cursor = db.query(Constants.TABLA_NAME_USERS, fields, Constants.TABLA_FIELD_NAME+"=?",params,null,null,null);
+            Cursor cursor = db.query(Constants.TABLA_NAME_USERS, fields, Constants.TABLA_FIELD_NAME + "=?", params, null, null, null);
             cursor.moveToFirst();
 
-            textViewName.setText(cursor.getString(0));
-            textViewPhone.setText(cursor.getString(1));
-            textViewEmail.setText(cursor.getString(2));
+            textViewName.setText(cursor.getString(1));
+            textViewPhone.setText(cursor.getString(2));
+            textViewEmail.setText(cursor.getString(3));
 
+
+            edtxtName.setText(cursor.getString(1));
+            edtxtphone.setText(cursor.getString(2));
+            edtxtEmail.setText(cursor.getString(3));
+            id = cursor.getInt(0);
             cursor.close();
-        }catch (Exception e){
+        } catch (Exception e) {
             Toast.makeText(this, "Nombre del contacto no encontrado", Toast.LENGTH_SHORT).show();
         }
+
+
     }
+
+    public void updateUsers(View v) {
+
+        SQLiteDatabase bdc = sqliteHelper.getReadableDatabase();
+
+        bdc.execSQL("UPDATE users SET name= '"+edtxtName.getText().toString()+"', phone= "+Integer.parseInt(edtxtphone.getText().toString())+", email = '"+edtxtEmail.getText().toString()+"' WHERE id = "+id);
+
+        Intent intent = new Intent(this, ContactsActivity.class);
+        startActivity(intent);
+
+    }
+
+    int id;
 }
